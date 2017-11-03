444c6bc5837e53afdf6fa2c146e1d437d90eb0c4
Alessio <alessio@alessio.com>
2017-11-03 09:05:05 +0000

    initial commit

---
+++ Quick Integration Example/ConfirmationViewController.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2017 Adyen B.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+    
+    @IBAction func tapButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
