98aa7390bcfc67ffb8173102036ab1d8b4430cef
Alessio <alessio@alessio.com>
2017-11-03 09:13:12 +0000

    changes to work with ivyspace api

--- Quick Integration Example/Configuration.swift
+++ Quick Integration Example/Configuration.swift
@@ -8,5 +8,7 @@ import Foundation
 
 // Fill in your app identifier and secret key here.
 struct Configuration {
-    static let appSecretKey = ""
+    static let api_url = "https://api.ivyspace.com/v1"
+    static let course_id = 1627
+    static let mobile_token = "d0f0d877cdad719c959ac866d57d7a43304aa305033904711074460e98ebb8458f5a473863a01f6c08bbd35a782cff35df1cff357f533ed831951c6ce5cfb88c"
 }
