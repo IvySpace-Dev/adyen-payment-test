b2b7e24d8035278137f7641c40ea6d9677a9b47c
mieszko4 <mieszko4@gmail.com>
2017-11-07 18:49:38 +0100

    Add -adyen urls scheme

--- Quick Integration Example/AppDelegate.swift
+++ Quick Integration Example/AppDelegate.swift
@@ -8,37 +8,37 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-    
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-    
+
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-    
+
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-    
+
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-    
+
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    
+
+
     //Function to show alert
     func showAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message:message, preferredStyle: UIAlertControllerStyle.alert)
@@ -49,16 +49,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         alertWindow.windowLevel = UIWindowLevelAlert + 1;
         alertWindow.makeKeyAndVisible()
         alertWindow.rootViewController?.present(alert, animated: true, completion: nil)
-        
+
     }
-    
+
     //modified function to keep track of sourceApplication
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         DispatchQueue.main.async {
             self.showAlert(title: "AppDelegate func application", message:"url="+url.absoluteString+" | sourceApplication="+sourceApplication!)
-            
-            if (url.scheme == "example-shopping-app"){
-                self.showAlert(title: "URL scheme detected", message: "URL Scheme is example-shopping-app")
+
+            if (url.scheme == "exampleshoppingapp"){
+                self.showAlert(title: "URL scheme detected", message: "URL Scheme is exampleshoppingapp")
+                if let viewController = self.window?.rootViewController as? ShoppingCartViewController {
+                    viewController.applicationDidReceive(url)
+                }
+            }
+
+            if (url.scheme == "exampleshoppingapp-adyen"){
+                self.showAlert(title: "URL scheme detected", message: "URL Scheme is exampleshoppingapp-adyen")
                 if let viewController = self.window?.rootViewController as? ShoppingCartViewController {
                     viewController.applicationDidReceive(url)
                 }
--- Quick Integration Example/ShoppingCartViewController.swift
+++ Quick Integration Example/ShoppingCartViewController.swift
@@ -8,20 +8,20 @@ import UIKit
 import Adyen
 
 class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelegate, CheckoutViewControllerCardScanDelegate, CardIOPaymentViewControllerDelegate {
-    
+
     @IBOutlet weak var contentImageView: UIImageView!
     @IBOutlet weak var actionButton: UIButton!
-    
+
     // MARK: - UIViewController
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     // MARK: - CheckoutViewControllerDelegate
-    
+
     func checkoutViewController(_ controller: CheckoutViewController, requiresPaymentDataForToken token: String, completion: @escaping DataCompletion) {
-        
+
         var getPriceRequest = URLRequest(url: URL(string: Configuration.api_url + "/collections/courses/" + String(Configuration.course_id))!)
         getPriceRequest.httpMethod = "GET"
         getPriceRequest.allHTTPHeaderFields = [
@@ -30,20 +30,20 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
             "Accept-Currency": "CNY",
             "Authorization":"MobileToken " + Configuration.mobile_token
         ]
-        
+
         let getPriceSession = URLSession.shared
-        
+
         getPriceSession.dataTask(with: getPriceRequest) {data, response, err in
             if let data = data,
                 let json = try? JSONSerialization.jsonObject(with: data, options: []) as! [String: Any] {
                 if let price = json["price"] {
                     if let currency = json["currency"] {
                         let dateformatter = DateFormatter()
-                        
+
                         dateformatter.dateFormat = "MM/dd/yy h:mm a Z"
-                        
+
                         let now = dateformatter.string(from: NSDate() as Date)
-                        
+
                         let paymentDetails: [String: Any] = [
                             "amount": [
                                 "value": price,
@@ -53,12 +53,12 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
                             "countryCode": "CN",
                             "shopperLocale": "zh-CN",
                             "shopperReference": "shopper@company.com",
-                            "returnUrl": "example-shopping-app://",
+                            "returnUrl": "exampleshoppingapp-adyen://",
                             "channel": "iOS",
                             "ivyspace_data": ["course_id":Configuration.course_id],
                             "token": token
                         ]
-            
+
                         var request = URLRequest(url: URL(string: Configuration.api_url + "/purchase_courses/adyen-sdk/")!)
                         request.httpMethod = "POST"
                         request.httpBody = try? JSONSerialization.data(withJSONObject: paymentDetails, options: [])
@@ -68,7 +68,7 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
                             "Accept-Currency": "CNY",
                             "Authorization":"MobileToken " + Configuration.mobile_token
                         ]
-                
+
                         let session = URLSession(configuration: .default)
                         session.dataTask(with: request) { data, response, error in
                             if let data = data {
@@ -80,18 +80,18 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
             }
         }.resume()
     }
-    
+
     func checkoutViewController(_ controller: CheckoutViewController, requiresReturnURL completion: @escaping URLCompletion) {
         appUrlCompletion = completion
     }
-    
+
     func checkoutViewController(_ controller: CheckoutViewController, didFinishWith result: PaymentRequestResult) {
         dismiss(animated: true)
-        
+
         var success: Bool = false
-        
+
         switch result {
-            
+
         case let .payment(payment):
             success = (payment.status == .received || payment.status == .authorised)
         case let .error(error):
@@ -102,20 +102,20 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
                 break
             }
         }
-        
+
         if success {
             presentSuccessScreen()
         } else {
             presentFailureScreen()
         }
     }
-    
+
     // MARK: - CheckoutViewControllerCardScanDelegate
-    
+
     func shouldShowCardScanButton(for checkoutViewController: CheckoutViewController) -> Bool {
         return true
     }
-    
+
     func scanCard(for checkoutViewController: CheckoutViewController, completion: @escaping CardScanCompletion) {
         if let scanViewController = CardIOPaymentViewController(paymentDelegate: self),
             let presented = presentedViewController {
@@ -126,23 +126,23 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
             completion((number: nil, expiryDate: nil, cvc: nil))
         }
     }
-    
+
     // MARK: - CardIOPaymentViewControllerDelegate
-    
+
     func userDidCancel(_ paymentViewController: CardIOPaymentViewController!) {
         if let presented = presentedViewController {
             presented.dismiss(animated: true)
         }
-        
+
         cardScanCompletion?((number: nil, expiryDate: nil, cvc: nil))
         cardScanCompletion = nil
     }
-    
+
     func userDidProvide(_ cardInfo: CardIOCreditCardInfo!, in paymentViewController: CardIOPaymentViewController!) {
         if let presented = presentedViewController {
             presented.dismiss(animated: true)
         }
-        
+
         let number = cardInfo.cardNumber
         let month = String(format: "%02d", cardInfo.expiryMonth)
         let year = String(format: "%02d", cardInfo.expiryYear % 100)
@@ -151,28 +151,28 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
         cardScanCompletion?((number: number, expiryDate: expiryDate, cvc: cvc))
         cardScanCompletion = nil
     }
-    
+
     // MARK: - Public
-    
+
     func applicationDidReceive(_ url: URL) {
         appUrlCompletion?(url)
     }
-    
+
     // MARK: - Private
-    
+
     private var appUrlCompletion: URLCompletion?
     private var cardScanCompletion: CardScanCompletion?
-    
+
     @IBAction private func checkout(_ sender: Any) {
         // Customize appearance of SDK.
         var appearance = AppearanceConfiguration.default
         appearance.tintColor = #colorLiteral(red: 0.4107530117, green: 0.8106812239, blue: 0.7224243283, alpha: 1)
-        
+
         let checkoutViewController = CheckoutViewController(delegate: self, appearanceConfiguration: appearance)
         checkoutViewController.cardScanDelegate = self
         present(checkoutViewController, animated: true)
     }
-    
+
     @objc
     private func presentInitialScreen() {
         UIView.transition(
@@ -185,19 +185,19 @@ class ShoppingCartViewController: UIViewController, CheckoutViewControllerDelega
         actionButton.removeTarget(self, action: #selector(presentInitialScreen), for: .touchUpInside)
         actionButton.addTarget(self, action: #selector(checkout(_:)), for: .touchUpInside)
     }
-    
+
     private func presentSuccessScreen() {
         contentImageView.image = #imageLiteral(resourceName: "Success")
         actionButton.setImage(#imageLiteral(resourceName: "back-to-shop"), for: .normal)
         actionButton.removeTarget(self, action: #selector(checkout(_:)), for: .touchUpInside)
         actionButton.addTarget(self, action: #selector(presentInitialScreen), for: .touchUpInside)
     }
-    
+
     private func presentFailureScreen() {
         contentImageView.image = #imageLiteral(resourceName: "Failure")
         actionButton.setImage(#imageLiteral(resourceName: "try-again"), for: .normal)
         actionButton.removeTarget(self, action: #selector(checkout(_:)), for: .touchUpInside)
         actionButton.addTarget(self, action: #selector(presentInitialScreen), for: .touchUpInside)
     }
-    
+
 }
